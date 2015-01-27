d268764173215fa3cb35e6efa8afa85792c0b487
moljac <mcvjetko@holisticware.net>
2015-01-27 08:59:38 +0100

    Project fixes for Xamarin.Forms v.1.3.1

---
+++ src/clients/mobile/HolisticWare.Ph4ct3x/HolisticWare.Ph4ct3x/Pages/About.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HolisticWare.Ph4ct3x
+{
+	public partial class About : ContentPage
+	{
+		public About ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+

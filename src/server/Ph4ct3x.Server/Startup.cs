2cd8bd3186b6a8ff417d1b615bc949596cbbcaad
moljac <mcvjetko@holisticware.net>
2015-02-10 09:36:15 +0100

    ASP.net v5 vNext application initial commit

---
+++ src/server/Ph4ct3x.Server/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+
+namespace Ph4ct3x.Server
+{
+    public class Startup
+    {
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+			return;
+		}
+		public void Configure(IApplicationBuilder app)
+        {
+			// For more information on how to configure your application, 
+			// visit http://go.microsoft.com/fwlink/?LinkID=398940
+
+			app.UseMvc();
+			app.UseWelcomePage();
+
+			return;
+		}
+
+	}
+}

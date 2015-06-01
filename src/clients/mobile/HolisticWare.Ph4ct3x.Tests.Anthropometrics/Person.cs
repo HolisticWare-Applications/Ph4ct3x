f3baec3bfd9b91a436300dd2d642362e55fed9cc
moljac <mcvjetko@holisticware.net>
2015-06-01 15:56:09 +0200

    UI/Acceptance tests

---
+++ src/clients/mobile/HolisticWare.Ph4ct3x.Tests.Anthropometrics/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HolisticWare.Ph4ct3x.Tests.Anthropometrics
+{
+	public partial class Person : HolisticWare.Ph4ct3x.Person
+	{
+		public Person ()
+		{
+		}
+
+		public double Height
+		{
+			get;
+			set;
+		}
+
+		public double Weight
+		{
+			get;
+			set;
+		}
+
+		public double Height
+		{
+			get;
+			set;
+		}
+
+	}
+}
+

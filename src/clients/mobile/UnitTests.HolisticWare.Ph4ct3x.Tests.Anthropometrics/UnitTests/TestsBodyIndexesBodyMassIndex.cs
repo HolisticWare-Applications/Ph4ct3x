37fc516bc6e6bea5b39b463e76a16bf927e4c37e
moljac <mcvjetko@holisticware.net>
2015-06-01 15:57:22 +0200

    unitTests Anthropometrics

---
+++ src/clients/mobile/UnitTests.HolisticWare.Ph4ct3x.Tests.Anthropometrics/UnitTests/TestsBodyIndexesBodyMassIndex.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System;
+using HolisticWare.Ph4ct3x.Tests.Anthropometrics;
+
+namespace UnitTests.HolisticWare.Ph4ct3x.Tests.Anthropometrics
+{
+	[TestFixture ()]
+	public partial class TestsBodyIndexesBodyMassIndex
+	{
+		// http://en.wikipedia.org/wiki/Body_mass_index
+
+		Person person = null;
+
+		[SetUp]
+		public void Setup ()
+		{
+			person =  new Person()
+			{
+			};
+
+			return;
+		}
+
+		
+		[TearDown]
+		public void Tear ()
+		{
+		}
+
+		[Test]
+		public void Pass ()
+		{
+			Console.WriteLine ("test1");
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Fail ()
+		{
+			Assert.False (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+
+		[Test]
+		public void Inconclusive ()
+		{
+			Assert.Inconclusive ("Inconclusive");
+		}
+	}
+}
+
---
+++ src/clients/mobile/UnitTests.HolisticWare.Ph4ct3x.Tests.Anthropometrics/UnitTests/TestsPerson.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using HolisticWare.Ph4ct3x.Tests.Anthropometrics;
+
+namespace UnitTests.HolisticWare.Ph4ct3x.Tests.Anthropometrics
+{
+	[TestFixture ()]
+	public partial class TestsPerson
+	{
+		Person person = null;
+
+		[SetUp]
+		public void Setup ()
+		{
+			person =  new Person()
+			{
+			  
+			};
+
+			return;
+		}
+
+		
+		[TearDown]
+		public void Tear ()
+		{
+		}
+
+		[Test]
+		public void Pass ()
+		{
+			Console.WriteLine ("test1");
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Fail ()
+		{
+			Assert.False (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+
+		[Test]
+		public void Inconclusive ()
+		{
+			Assert.Inconclusive ("Inconclusive");
+		}
+	}
+}
+

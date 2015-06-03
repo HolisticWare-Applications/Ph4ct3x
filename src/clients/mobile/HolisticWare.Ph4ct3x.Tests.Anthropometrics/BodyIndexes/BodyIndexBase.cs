5a9ecd1ed0668157113f1dc6b73f2cb2ebc9a5ee
moljac <mcvjetko@holisticware.net>
2015-06-03 08:58:38 +0200

    Tests added

---
+++ src/clients/mobile/HolisticWare.Ph4ct3x.Tests.Anthropometrics/BodyIndexes/BodyIndexBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HolisticWare.Ph4ct3x.Tests.Anthropometrics
+{
+	public abstract partial class BodyIndexBase : HolisticWare.Ph4ct3x.Object
+	{
+		public BodyIndexBase ()
+		{
+		}
+
+		public Person Person
+		{
+			get;
+			set;
+		}
+
+		public virtual double Value
+		{
+			get
+			{
+				return Double.NaN;
+			}
+		}
+
+
+	}
+}
+

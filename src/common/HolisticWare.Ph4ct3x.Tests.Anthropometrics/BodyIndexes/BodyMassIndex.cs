57da13937296515e832fc728906755897685a1c8
moljac <mcvjetko@holisticware.net>
2015-06-04 23:47:30 +0200

    Domain Logic test moved

---
+++ src/common/HolisticWare.Ph4ct3x.Tests.Anthropometrics/BodyIndexes/BodyMassIndex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HolisticWare.Ph4ct3x.Tests.Anthropometrics
+{
+	public partial class BodyMassIndex : BodyIndexBase
+	{
+		public BodyMassIndex ()
+		{
+		}
+
+		public override double Value
+		{
+			get
+			{
+				
+				return Double.NaN;
+			}
+		}
+
+		public double Height
+		{
+			get;
+			set;
+		}
+
+		public double Weight
+		{
+			get;
+			set;
+		}
+	}
+}
+
---
+++ src/common/HolisticWare.Ph4ct3x.Tests.Motorics/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolisticWare.Ph4ct3x.Tests.Motorics
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+

c1df11202e604b710ce6e4ba4deb4cdcb31bfbce
moljac <mcvjetko@holisticware.net>
2015-06-04 23:51:31 +0200

    unit tests mobile moved

---
+++ tests/UnitTests/clients/mobile/UnitTests.UnitTests.HolisticWare.Ph4ct3x.Tests.Anthropometrics.XamarinAndroid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace UnitTests.UnitTests.HolisticWare.Ph4ct3x.Tests.Anthropometrics.XamarinAndroid
+{
+	[Activity (Label = "UnitTests.UnitTests.HolisticWare.Ph4ct3x.Tests.Anthropometrics.XamarinAndroid", MainLauncher = true)]
+	public class MainActivity : TestSuiteActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			// tests can be inside the main assembly
+			AddTest (Assembly.GetExecutingAssembly ());
+			// or in any reference assemblies
+			// AddTest (typeof (Your.Library.TestClass).Assembly);
+
+			// Once you called base.OnCreate(), you cannot add more assemblies.
+			base.OnCreate (bundle);
+		}
+	}
+}
+

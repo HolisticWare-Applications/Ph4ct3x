f0d846dc8da4201cf54c7dc08702ca1d5aafd589
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-06-22 09:31:33 +0200

    Xamarin.Froms client added

---
+++ samples/client/HolisticWare.Ph4ct3x.Judo/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HolisticWare.Ph4ct3x.Judo.Models;
+
+namespace HolisticWare.Ph4ct3x.Judo.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

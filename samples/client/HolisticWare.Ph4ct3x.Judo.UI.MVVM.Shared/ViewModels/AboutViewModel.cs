e0c0344336683ac0f6824933a9e3ed80470e712a
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2018-07-06 08:49:38 +0200

    forms sample

---
+++ samples/client/HolisticWare.Ph4ct3x.Judo.UI.MVVM.Shared/ViewModels/AboutViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+using HolisticWare.Ph4ct3x.Judo.Views;
+
+namespace HolisticWare.Ph4ct3x.Judo.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+
+            OpenWebCommand = new Command(() => Device.OpenUri(new Uri("https://xamarin.com/platform")));
+            //Navigation = 
+            OpenSearchPageCommand = new Command
+                (
+                    async () => 
+                    Device.OpenUri(new Uri("https://xamarin.com/platform"))
+                    //await Application.Current.MainPage.Navigation.PushAsync(new SearchJudoka())
+                );
+        }
+
+        public INavigation Navigation { get; set; }
+
+        public ICommand OpenWebCommand { get; }
+
+        public ICommand OpenSearchPageCommand { get; }
+    }
+}
---
+++ samples/client/HolisticWare.Ph4ct3x.Judo.UI.MVVM.Shared/Views/SearchJudoka.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HolisticWare.Ph4ct3x.Judo.Views
+{
+    public partial class SearchJudoka : ContentPage
+    {
+        public SearchJudoka()
+        {
+            InitializeComponent();
+
+            buttonSearchJudoka.Clicked += ButtonSearchJudoka_Clicked;
+        }
+
+        void ButtonSearchJudoka_Clicked(object sender, EventArgs e)
+        {
+            string name = this.entryNameJudoka.Text;
+            string gender = this.entryGender.Text;
+            string country = this.entryCountry.Text;
+
+            // https://www.ijf.org/judoka?name=Cvjetko&nation=CRO&gender=both&category=cad
+            // https://www.ijf.org/judoka?name=bozkurt&nation=all&gender=f&category=all
+
+
+            return;
+        }
+
+    }
+}
